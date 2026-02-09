e5281fdc823e58d86dafbfd6c2b736ac83060f94
Vyrubaev <58784103+vyrubaev@users.noreply.github.com>
2026-02-09 21:29:32 +0400

    test выкладывания сайта через копирование всех файлов из проекта

---
+++ src/components/Footer.jsx
@@ -0,0 +1,7 @@
+export const Footer = () => {
+  return (
+    <footer className="footer">
+      <p>© 2026 My Company. All rights reserved.</p>
+    </footer>
+  )
+}
---
+++ src/components/Navbar.jsx
@@ -0,0 +1,31 @@
+import { Link } from 'react-router-dom'
+export const Navbar = () => {
+  return (
+    <nav className="navbar">
+      <ul>
+        <li className="nav-item">
+          <button aria-expanded="false">Apps</button>
+          <ul className="sub-menu" aria-label="Apps">
+            <li><Link to="/jungling">Jungling</Link></li>
+            <li><Link to="/jungling">Chat</Link></li>
+            <li><Link to="/jungling">Email</Link></li>
+          </ul></li>
+        <li className="nav-item">
+          <button aria-expanded="false">Apps</button>
+          <ul className="sub-menu" aria-label="Apps">
+            <li><Link to="/jungling">Jungling</Link></li>
+            <li><Link to="/jungling">Chat</Link></li>
+            <li><Link to="/jungling">Email</Link></li>
+          </ul></li>
+        <li className="nav-item">
+          <button aria-expanded="false">Apps</button>
+          <ul className="sub-menu" aria-label="Apps">
+            <li><Link to="/jungling">Jungling</Link></li>
+            <li><Link to="/jungling">Chat</Link></li>
+            <li><Link to="/jungling">Email</Link></li>
+          </ul>
+        </li>
+      </ul>
+    </nav>
+  )
+}
